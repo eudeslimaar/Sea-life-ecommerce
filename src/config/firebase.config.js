88701bfe2bd9f205070420f0e8c097ae2fb51ac2
eudeslimaar <eudesfilho1994@outlook.com>
2022-03-06 15:11:20 -0300

    Firebase key

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,10 +1,11 @@
 
   export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyBXLUoxtL8NOXvo9a6QZfgnVHbvBOMXjwU",
+    authDomain: "sea-life-c45b3.firebaseapp.com",
+    databaseURL: "https://sea-life-c45b3-default-rtdb.firebaseio.com",
+    projectId: "sea-life-c45b3",
+    storageBucket: "sea-life-c45b3.appspot.com",
+    messagingSenderId: "946702104087",
+    appId: "1:946702104087:web:a7ba698fbfb0de42f08e52"
   };
   
